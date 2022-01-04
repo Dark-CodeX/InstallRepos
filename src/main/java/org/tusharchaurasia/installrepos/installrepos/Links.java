7d44bf82bcf4c3e866d8b60c18b9d005dd12260c
Tushar Chaurasia <77012813+Dark-CodeX@users.noreply.github.com>
2022-01-04 17:23:48 +0530

    links of github repos files

---
+++ src/main/java/org/tusharchaurasia/installrepos/installrepos/Links.java
@@ -0,0 +1,44 @@
+/*
+MIT License
+
+Copyright (c) 2021 Tushar Chaurasia
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+package org.tusharchaurasia.installrepos.installrepos;
+
+public class Links {
+        public static String[] sstring_urls = {
+                        "https://raw.githubusercontent.com/Dark-CodeX/sstring/master/sstring/sstring.h",
+                        "https://raw.githubusercontent.com/Dark-CodeX/SafeString/main/sstring/prototype_err.h",
+                        "https://raw.githubusercontent.com/Dark-CodeX/SafeString/main/sstring/binary.h",
+                        "https://raw.githubusercontent.com/Dark-CodeX/SafeString/main/sstring/morse_code.h",
+                        "https://raw.githubusercontent.com/Dark-CodeX/SafeString/main/sstring/sstring.hh" };
+
+        public static String[] vector_urls = {
+                        "https://raw.githubusercontent.com/Dark-CodeX/vector/main/vector/vector.h",
+                        "https://raw.githubusercontent.com/Dark-CodeX/vector/main/vector/vector.hh" };
+
+        public static String[] map_urls = { "https://raw.githubusercontent.com/Dark-CodeX/map/master/map/map.hh" };
+
+        public static String[] returns_urls = {
+                        "https://raw.githubusercontent.com/Dark-CodeX/returns/master/returns/default.hh",
+                        "https://raw.githubusercontent.com/Dark-CodeX/returns/master/returns/optional.hh" };
+}
