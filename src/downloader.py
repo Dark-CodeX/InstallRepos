6307e3848fb10d902432b1d84fd85156f2395cb6
Tushar Chaurasia <77012813+Dark-CodeX@users.noreply.github.com>
2023-05-13 20:38:55 +0530

    Made it work on windows without any ext. libs

--- src/downloader.py
+++ src/downloader.py
@@ -2,21 +2,28 @@
 # author: Tushar Chaurasia (Dark-CodeX)
 # license: This file is licensed under the GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007. You may obtain a copy of this license at https://www.gnu.org/licenses/gpl-3.0.en.html.
 
-import requests
+import urllib.request
+import urllib.error
 import tkinter as tk
+
 
 class downloader:
     @staticmethod
     def download_file(url: str, location: str, txt_box):
-        response = requests.get(url)
-        if response.status_code == 200:
-            with open(location, 'wb') as file:
-                file.write(response.content)
-            return True
+        response = urllib.request.urlopen(url)
+        if response.status == 200:
+            try:
+                with open(location, "wb") as file:
+                    file.write(response.read())
+                return True
+            except Exception as e:
+                print(f"err: could not save the file at {location}: {str(e)}")
+                return False
         else:
             if txt_box == None:
                 print(f"err: could not download file '{url}'")
                 return False
             else:
-                txt_box.insert(tk.END, f"err: could not download the file '{url}'\n")
+                txt_box.insert(
+                    tk.END, f"err: could not download the file '{url}'\n")
                 return False
