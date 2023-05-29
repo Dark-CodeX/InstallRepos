09e614b18e26b197e9bfee100ca34f1f7a609862
Tushar Chaurasia <77012813+Dark-CodeX@users.noreply.github.com>
2023-05-29 15:51:31 +0530

    updated links

--- src/links.py
+++ src/links.py
@@ -4,53 +4,53 @@
 
 class repos_links:
     sstring_urls = [
-        "https://raw.githubusercontent.com/Dark-CodeX/sstring/main/sstring/sstring_boolean.hh",
-        "https://raw.githubusercontent.com/Dark-CodeX/sstring/main/sstring/sstring_class.hh",
-        "https://raw.githubusercontent.com/Dark-CodeX/sstring/main/sstring/sstring_conversion.hh",
-        "https://raw.githubusercontent.com/Dark-CodeX/sstring/main/sstring/sstring_crypto.hh",
-        "https://raw.githubusercontent.com/Dark-CodeX/sstring/main/sstring/sstring_cstring.hh",
-        "https://raw.githubusercontent.com/Dark-CodeX/sstring/main/sstring/sstring_ctor.hh",
-        "https://raw.githubusercontent.com/Dark-CodeX/sstring/main/sstring/sstring_find.hh",
-        "https://raw.githubusercontent.com/Dark-CodeX/sstring/main/sstring/sstring_formatting.hh",
-        "https://raw.githubusercontent.com/Dark-CodeX/sstring/main/sstring/sstring.hh",
-        "https://raw.githubusercontent.com/Dark-CodeX/sstring/main/sstring/sstring_io.hh",
-        "https://raw.githubusercontent.com/Dark-CodeX/sstring/main/sstring/sstring_iterators.hh",
-        "https://raw.githubusercontent.com/Dark-CodeX/sstring/main/sstring/sstring_lexer_split.hh",
-        "https://raw.githubusercontent.com/Dark-CodeX/sstring/main/sstring/sstring_manipulation.hh",
-        "https://raw.githubusercontent.com/Dark-CodeX/sstring/main/sstring/sstring_memory.hh",
-        "https://raw.githubusercontent.com/Dark-CodeX/sstring/main/sstring/sstring_metrics.hh",
-        "https://raw.githubusercontent.com/Dark-CodeX/sstring/main/sstring/sstring_operators.hh",
-        "https://raw.githubusercontent.com/Dark-CodeX/sstring/main/sstring/sstring_remove.hh",
-        "https://raw.githubusercontent.com/Dark-CodeX/sstring/main/sstring/sstring_replace.hh",
-        "https://raw.githubusercontent.com/Dark-CodeX/sstring/main/sstring/sstring_set_get_append.hh",
-        "https://raw.githubusercontent.com/Dark-CodeX/sstring/main/sstring/sstring_static.hh"]
+        "https://raw.githubusercontent.com/Dark-CodeX/openutils/main/sstring/sstring_boolean.hh",
+        "https://raw.githubusercontent.com/Dark-CodeX/openutils/main/sstring/sstring_class.hh",
+        "https://raw.githubusercontent.com/Dark-CodeX/openutils/main/sstring/sstring_conversion.hh",
+        "https://raw.githubusercontent.com/Dark-CodeX/openutils/main/sstring/sstring_crypto.hh",
+        "https://raw.githubusercontent.com/Dark-CodeX/openutils/main/sstring/sstring_cstring.hh",
+        "https://raw.githubusercontent.com/Dark-CodeX/openutils/main/sstring/sstring_ctor.hh",
+        "https://raw.githubusercontent.com/Dark-CodeX/openutils/main/sstring/sstring_find.hh",
+        "https://raw.githubusercontent.com/Dark-CodeX/openutils/main/sstring/sstring_formatting.hh",
+        "https://raw.githubusercontent.com/Dark-CodeX/openutils/main/sstring/sstring.hh",
+        "https://raw.githubusercontent.com/Dark-CodeX/openutils/main/sstring/sstring_io.hh",
+        "https://raw.githubusercontent.com/Dark-CodeX/openutils/main/sstring/sstring_iterators.hh",
+        "https://raw.githubusercontent.com/Dark-CodeX/openutils/main/sstring/sstring_lexer_split.hh",
+        "https://raw.githubusercontent.com/Dark-CodeX/openutils/main/sstring/sstring_manipulation.hh",
+        "https://raw.githubusercontent.com/Dark-CodeX/openutils/main/sstring/sstring_memory.hh",
+        "https://raw.githubusercontent.com/Dark-CodeX/openutils/main/sstring/sstring_metrics.hh",
+        "https://raw.githubusercontent.com/Dark-CodeX/openutils/main/sstring/sstring_operators.hh",
+        "https://raw.githubusercontent.com/Dark-CodeX/openutils/main/sstring/sstring_remove.hh",
+        "https://raw.githubusercontent.com/Dark-CodeX/openutils/main/sstring/sstring_replace.hh",
+        "https://raw.githubusercontent.com/Dark-CodeX/openutils/main/sstring/sstring_set_get_append.hh",
+        "https://raw.githubusercontent.com/Dark-CodeX/openutils/main/sstring/sstring_static.hh"]
 
     vector_urls = [
-        "https://raw.githubusercontent.com/Dark-CodeX/vector/main/vector/vector.hh"]
+        "https://raw.githubusercontent.com/Dark-CodeX/openutils/main/vector/vector.hh"]
 
     map_urls = [
-        "https://raw.githubusercontent.com/Dark-CodeX/map/master/map/map.hh"]
+        "https://raw.githubusercontent.com/Dark-CodeX/openutils/master/map/map.hh"]
 
     optional_urls = [
-        "https://raw.githubusercontent.com/Dark-CodeX/optional/master/optional/optional.hh"]
+        "https://raw.githubusercontent.com/Dark-CodeX/openutils/master/optional/optional.hh"]
 
     array_urls = [
-        "https://raw.githubusercontent.com/Dark-CodeX/array/master/array/array.hh"]
+        "https://raw.githubusercontent.com/Dark-CodeX/openutils/master/array/array.hh"]
 
     date_time_urls = [
-        "https://raw.githubusercontent.com/Dark-CodeX/date-time/main/date-time/date.hh",
-        "https://raw.githubusercontent.com/Dark-CodeX/date-time/main/date-time/time.hh"]
+        "https://raw.githubusercontent.com/Dark-CodeX/openutils/main/date-time/date.hh",
+        "https://raw.githubusercontent.com/Dark-CodeX/openutils/main/date-time/time.hh"]
 
     heap_pair_urls = [
-        "https://raw.githubusercontent.com/Dark-CodeX/heap-pair/master/heap-pair/heap-pair.hh"]
+        "https://raw.githubusercontent.com/Dark-CodeX/openutils/master/heap-pair/heap-pair.hh"]
 
     chunkio_urls = [
-        "https://raw.githubusercontent.com/Dark-CodeX/chunkio/main/chunkio/chunkio_bytes_reader.hh",
-        "https://raw.githubusercontent.com/Dark-CodeX/chunkio/main/chunkio/chunkio_lines_reader.hh",
-        "https://raw.githubusercontent.com/Dark-CodeX/chunkio/main/chunkio/chunkio_writer.hh"]
+        "https://raw.githubusercontent.com/Dark-CodeX/openutils/main/chunkio/chunkio_bytes_reader.hh",
+        "https://raw.githubusercontent.com/Dark-CodeX/openutils/main/chunkio/chunkio_lines_reader.hh",
+        "https://raw.githubusercontent.com/Dark-CodeX/openutils/main/chunkio/chunkio_writer.hh"]
 
     mthread_urls = [
-        "https://raw.githubusercontent.com/Dark-CodeX/mthread/main/mthread/mthread.hh"]
+        "https://raw.githubusercontent.com/Dark-CodeX/openutils/main/mthread/mthread.hh"]
 
     @staticmethod
     def get_urls(url: str):
